5509c2b2b814c98f066b07d51bd28f363121be8c
Elly <professionellys@gmail.com>
2025-10-22 15:24:39 -0400

    Assignment Completion

--- OneDrive/Desktop/College/Fall 2025/Advanced Programming Techniques/Assignments/Project Three/Array.h
+++ OneDrive/Desktop/College/Fall 2025/Advanced Programming Techniques/Assignments/Project Three/Array.h
@@ -1,8 +1,8 @@
+void init(double *&arr, int size);
 void insert(int index, double num, double *&arr, int &size);
 void remove(int index, double *arr, int &size);
-int get(int index, double *arr, int size);
 void clear(double *&arr, int &size);
+void print(double *arr, int size);
+int get(int index, double *arr, int size);
 int find(double num, double *arr, int size);
 bool equals(double *arr1, int size1, double *arr2, int size2);
-void init(double *&arr, int size);
-void print(double *arr, int size);
