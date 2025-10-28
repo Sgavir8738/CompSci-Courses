b3cf64feef225d92611159a48d0fc50aeb04fb45
Elly <professionellys@gmail.com>
2025-10-28 15:16:47 -0400

    Assignment Push

---
+++ OneDrive/Desktop/College/Fall 2025/Advanced Programming Techniques/Assignments/Project Zero/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+    Author: Sebastian Gaviria 
+*/
+
+int main(int argc, char* argv[]) {
+
+    string first;
+    string last;
+    int year;
+    string studentClass;
+
+    cout << "Enter your first name: ";
+    cin >> first;
+
+    cout << "Enter your last name: ";
+    cin >> last;
+
+    cout << "How many years have you been at USC? ";
+    cin >> year;
+
+    switch (year) {
+        case 1:
+            studentClass = "Freshman";
+            break;
+        case 2:
+            studentClass = "Sophomore";
+            break;
+        case 3:
+            studentClass = "Junior";
+            break;
+        case 4:
+            studentClass = "Senior";
+            break;
+        default:
+            studentClass = "Graduate";
+    }
+
+    cout << first << " " << last << ", " << studentClass << endl;
+}
